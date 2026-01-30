1e10cf5c1da5c04bda3eff8e4bba2f01b2081134
H1712-bit <singhpardeepnirman96@gmail.com>
2026-01-30 11:38:36 +0530

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,184 @@
+  /*#include <stdio.h>
+int main()
+{
+    printf("Hello World \n");
+    printf("Hello World \n");
+    printf("Hello World \n");
+    return 0;
+}
+*/
+// OUTPUT
+/*# include <stdio.h>
+int main(){
+    int age=20;
+    float pi=3.14;
+    char star='*';
+    printf("Age is %d\n",age);
+    printf("Pi value is %f \n",pi);
+    printf("Star is %c",star);
+    return 0;
+}
+*/
+// INPUT IN C
+/*# include <stdio.h>
+int main(){
+    int age;
+    printf("Enter the age \n");
+    scanf("%d",&age);
+    printf("Age is : %d",age);
+    return 0;
+
+}
+*/
+// SUM OF TWO NUMBERS
+ /*# include <stdio.h>
+int main(){
+    int a , b;
+    printf("Enter the a \n");
+    scanf("%d",&a);
+    printf("Enter the b \n");
+    scanf("%d",&b);
+    int sum = a + b;
+    printf("Sum is %d",sum);
+    return 0;
+}
+*/
+// METHOD 2 FOR SUM 
+ /*# include <stdio.h>
+int main(){
+    int a , b;
+    printf("Enter the a \n");
+    scanf("%d",&a);
+    printf("Enter the b \n");
+    scanf("%d",&b);
+    printf("Sum is %d",a+b);
+    return 0;
+}
+*/
+// WAP TO CALCULATE THE AREA OF SQUARE
+ /*#include <stdio.h>
+int main(){
+    int a= 20;
+    int area= a*a;
+    printf("Area is  %d ", area);
+    return 0;
+}
+*/
+// M2 FOR TO CALUCATE THE AREA 
+// # include <stdio.h>
+// int main(){
+//     int sides;
+//     printf("Enter the sides \n");
+//     scanf("%d",&sides);
+//     int area = sides*sides;
+//     printf("Area of Square %d",area);
+//     return 0;
+// }  
+// CALCULATE THE SIMPLE INTEREST
+// # include <stdio.h>
+// int main(){
+//     float p,r,t,si;
+//     p=100;
+//     r=10;
+//     t=2;
+//     si=(p*r*t)/100;
+//     print("%f",si);
+//     return 0;
+// }
+// Ques 1
+// #include <stdio.h>
+// int main()
+// {
+//     int a = 15;
+//     printf("%d",a);
+//     return 0;
+// }
+
+// Ques 2
+// #include <stdio.h>
+// int main(){
+//     int a ;
+//     printf("Enter the number:");
+//     scanf("%d",&a);
+//     printf("%d",a);
+//     return 0;
+// }
+
+// Ques 8
+// #include <stdio.h>
+// int main(){
+//     int a,b;
+//     printf("Enter the num\n");
+//     scanf("%d",&a);
+//     printf("Enter the num:\n");
+//     scanf("%d",&b);
+//     int sum =a+b;
+//     printf("The sum is :%d",sum);
+//     return 0;
+
+// }
+// Ques 4 
+// #include <stdio.h>
+// int main(){
+//     char name [50];
+//     printf("Enter the name:\n");
+//     scanf("%s",&name);
+//     char month[20];
+//     printf("Enter the month\n");
+//     scanf("%s",&month);
+//     int date,year;
+//     printf("Enter the date \n ");
+//     scanf("%d",&date);
+//     printf("Enter the  year\n");
+//     scanf("%d",&year);
+//     long long  mobile;
+//     printf("Enter  the mobile\n ");
+//     scanf("%lld",&mobile);
+//     printf("Name: %s\nDOB: %s %d, %d\nMobile: %lld\n", name, month, date, year, mobile);
+
+//     return 0;
+// } 
+
+// Ques 6
+// #include <stdio.h>
+// int main(){
+//     float radius;
+//     printf("Enter the value :\n");
+//     scanf("%f",&radius);
+//     float pi= 3.14;
+//     float vol= 4/3*pi*radius*radius*radius;
+//     printf("Volume of sphere %f", vol);
+//     return 0; 
+// }
+// Ques 7
+// #include <stdio.h>
+// int main(){
+//     int a , b;
+//     printf("Enter the num1\n");
+//     scanf("%d",&a);
+//     printf("Enter the num2\n");
+//     scanf("%d",&b);
+//     printf("The perimeter of rectangle : %d", 2*(a+b));
+//     return 0;
+// }
+// Ques 3
+// #include <stdio.h>
+// int main(){
+//     int a =2;
+//     int b = 3;
+//     printf("The sum : %d",a+b);
+//     return 0;
+// }
+
+
+
+|
+
+
+
+
+
+
+
+
+
